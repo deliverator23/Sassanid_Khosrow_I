2f9b4cc3e557c347bf774815f274fe78a51f3a68
deliverator23 <iain.millar@gmail.com>
2020-04-08 17:14:10 +0100

    Fix text file.

--- Sassanid_Khosrow_I/Sassanid_Khosrow_I_Text.sql
+++ Sassanid_Khosrow_I/Sassanid_Khosrow_I_Text.sql
@@ -7,8 +7,5 @@ WHERE Tag = 'LOC_PEDIA_LEADERS_PAGE_LEADER_SUK_KHOSROW_I_CHAPTER_DETAILED_BODY' 
 UPDATE LocalizedText SET Text = "+3% bonus to [ICON_GreatPerson] Great Person point generation for each Wonder you own. Wiskar Districts gain +1 [ICON_Production] Production, +1 [ICON_Gold] Gold for each adjacent Wonder."  
 WHERE Tag = 'LOC_TRAIT_LEADER_SUK_ARCHWAY_OF_CTESIPHON_DESCRIPTION' AND Language = 'en_US';
 
-("",  																		"en_US",
-		)
-
 UPDATE LocalizedText SET Text = "Hail, Khosrow I, the Immortal Soul, King of Kings! Your fearlessness, ambition and learning are renowned throughout the world. Build your empire into a beacon of civilization by creating lush gardens, constructing great wonders, and attracting the world's most brilliant minds. Do this, and all the world will stand in awe at the glory of Eranshahr."
 WHERE Tag = 'LOC_LOADING_INFO_LEADER_SUK_KHOSROW_I' AND Language = 'en_US';
