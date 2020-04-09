579456c104edc40a8741a6d090c8a9f0493b87f4
deliverator23 <iain.millar@gmail.com>
2020-04-09 06:05:34 +0100

    Fix missing bracket.

--- Sassanid_Khosrow_I/Sassanid_Khosrow_I_Gameplay.sql
+++ Sassanid_Khosrow_I/Sassanid_Khosrow_I_Gameplay.sql
@@ -18,12 +18,12 @@ VALUES ('Sass_Khos_I_Wonder_Gold', "LOC_DISTRICT_CVS_HUNTINGPARK_WONDER_GOLD", "
 INSERT INTO Adjacency_YieldChanges (ID, Description, YieldType, YieldChange, TilesRequired, AdjacentWonder)
 VALUES ('Sass_Khos_I_Wonder_Production', "LOC_DISTRICT_CVS_HUNTINGPARK_WONDER_PRODUCTION", "YIELD_PRODUCTION", 1, 1, 1);
 
-INSERT INTO ExcludedAdjacencies ('YieldChangeId', 'TraitType'
+INSERT INTO ExcludedAdjacencies ('YieldChangeId', 'TraitType')
 SELECT 'Sass_Khos_I_Wonder_Gold', TraitType
 FROM   Traits
 WHERE  TraitType IN ('TRAIT_LEADER_CVS_KAWAD_I_TTBAM','TRAIT_LEADER_CVS_SHAPUR_I_UA', 'TRAIT_LEADER_CVS_BORAN_UA');
 
-INSERT INTO ExcludedAdjacencies ('YieldChangeId', 'TraitType'
+INSERT INTO ExcludedAdjacencies ('YieldChangeId', 'TraitType')
 SELECT 'Sass_Khos_I_Wonder_Production', TraitType
 FROM   Traits
 WHERE  TraitType IN ('TRAIT_LEADER_CVS_KAWAD_I_TTBAM','TRAIT_LEADER_CVS_SHAPUR_I_UA', 'TRAIT_LEADER_CVS_BORAN_UA');
