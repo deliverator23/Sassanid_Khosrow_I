537c85a4670121c82e2022c11719875e5e761b05
deliverator23 <iain.millar@gmail.com>
2020-04-16 10:07:46 +0100

    Tweak Colours.

--- Sassanid_Khosrow_I/Sassanid_Khosrow_I_Color.sql
+++ Sassanid_Khosrow_I/Sassanid_Khosrow_I_Color.sql
@@ -1,14 +1,15 @@
 INSERT OR REPLACE INTO Colors
 		(Type,										Color)
-VALUES	("COLOR_PLAYER_SUK_KHOSROW_I_PRIMARY",		"93,105,70,255"),   --#5D6946
-		("COLOR_PLAYER_SUK_KHOSROW_I_SECONDARY",	"224,193,121,255");	--#E0C179
+VALUES	("COLOR_PLAYER_SUK_KHOSROW_I_PRIMARY",		"93,105,70,255");   --#5D6946
 
 UPDATE PlayerColors
-SET     Alt1PrimaryColor = "COLOR_CVS_SASSANID_B_PRIMARY",
-        Alt1SecondaryColor = "COLOR_PLAYER_SUK_KHOSROW_I_SECONDARY",
-        Alt2PrimaryColor = "COLOR_PLAYER_SUK_KHOSROW_I_SECONDARY",
+SET     PrimaryColor = "COLOR_PLAYER_SUK_KHOSROW_I_PRIMARY",
+		SecondaryColor = "COLOR_CVS_SASSANID_C_SECONDARY",  -- Kawad Secondary
+		Alt1PrimaryColor = "COLOR_CVS_SASSANID_B_PRIMARY", -- Boran Primary
+        Alt1SecondaryColor = "COLOR_CVS_SASSANID_C_SECONDARY",
+        Alt2PrimaryColor = "COLOR_CVS_SASSANID_C_SECONDARY",
         Alt2SecondaryColor = "COLOR_PLAYER_SUK_KHOSROW_I_PRIMARY",
         Alt3PrimaryColor = "COLOR_PLAYER_SUK_KHOSROW_I_PRIMARY",
-        Alt3SecondaryColor = "COLOR_CVS_SASSANID_A_SECONDARY"
+        Alt3SecondaryColor = "COLOR_CVS_SASSANID_A_SECONDARY"  -- Shapur Secondary
 WHERE  Type = 'LEADER_SUK_KHOSROW_I'
 AND    Usage = 'Unique';
